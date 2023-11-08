91680c6ace1846cd3a463aa9393649e711a2a47a
Mathis Pitot <mpitot@z1r4p4.42lyon.fr>
2023-11-08 14:14:45 +0100

    push fd_ func's

---
+++ 42_cursus/libft/srcs/ft_putnbr_fd.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpitot <mpitot@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 14:03:46 by mpitot            #+#    #+#             */
+/*   Updated: 2023/11/08 14:06:18 by mpitot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd_unsigned(unsigned int n, int fd)
+{
+	if (n < 10)
+		ft_putchar_fd(n + 48, fd);
+	else
+	{
+		ft_putnbr_fd_unsigned(n / 10, fd);
+		ft_putchar_fd(n % 10, fd);
+	}
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd)
+		ft_putnbr_fd_unsigned(n * -1, fd);
+	}
+	else
+		ft_putnbr_fd_unsigned(n, fd);
+}
---
+++ 42_cursus/libft/srcs/ft_strlcat.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpitot <mpitot@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 13:01:59 by mpitot            #+#    #+#             */
+/*   Updated: 2023/11/08 13:58:55 by mpitot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = ft_strlen(dst);
+	while (src[i] && i < size - 1)
+	{
+		dst[i + j] = src[i];
+		i++;
+	}
+	dst[i + j] = '\0';
+	return (j + size)
+}
